e12eba5a76c357f99de92f4330b43e00b27a3765
AchrefHamdouni <113561979+AchrefHamdouni@users.noreply.github.com>
2023-06-19 16:13:18 +0100

    lists

--- admin-dashboard/src/pages/index.js
+++ admin-dashboard/src/pages/index.js
@@ -1,23 +1,140 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-const inter = Inter({ subsets: ["latin"] });
+import { Chart } from "chart.js/auto";
+import axios from "axios";
 
-export default function Home() {
+const Home = () => {
+  const chartRefs = useRef([]);
+  const [doctorsData, setDoctorsData] = useState(null);
+  const [usersData, setUsersData] = useState(null);
+  const [patientsData, setPatientsData] = useState(null);
+
+  useEffect(() => {
+    fetchDoctorsData();
+    fetchUsersData();
+    fetchPatientsData();
+  }, []);
+
+  const fetchDoctorsData = () => {
+    axios
+      .get("http://localhost:8080/admin/doctors")
+      .then((res) => {
+        const formattedData = {
+          labels: res.data.doctors.map((doctor) => doctor.specialization),
+          datasets: [
+            {
+              label: "Number of Doctors",
+              data: res.data.doctors.map((doctor) => doctor.count),
+              backgroundColor: "rgba(75, 192, 192, 0.6)",
+            },
+          ],
+        };
+        setDoctorsData(formattedData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const fetchUsersData = () => {
+    axios
+      .get("http://localhost:8080/admin/users")
+      .then((res) => {
+        const formattedData = {
+          labels: ["Users"],
+          datasets: [
+            {
+              label: "Number of Users",
+              data: [res.data.count],
+              backgroundColor: "rgba(54, 162, 235, 0.6)",
+            },
+          ],
+        };
+        setUsersData(formattedData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const fetchPatientsData = () => {
+    axios
+      .get("http://localhost:8080/admin/patients")
+      .then((res) => {
+        const formattedData = {
+          labels: ["Patients"],
+          datasets: [
+            {
+              label: "Number of Patients",
+              data: [res.data.count],
+              backgroundColor: "rgba(255, 99, 132, 0.6)",
+            },
+          ],
+        };
+        setPatientsData(formattedData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    if (doctorsData) {
+      const charts = chartRefs.current.map((chartRef, index) => {
+        const ctx = chartRef.getContext("2d");
+        const chart = new Chart(ctx, {
+          type: "doughnut",
+          data: doctorsData.datasets[index],
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+          },
+        });
+
+        return chart;
+      });
+
+      return () => {
+        charts.forEach((chart) => chart.destroy());
+      };
+    }
+  }, [doctorsData]);
+
   return (
-    <main>
-      <h1>Hello</h1>
-      <button>
-        <Link href="/users">Users</Link>
-      </button><br />
-      <button>
-        <Link href="/doctors">Doctors</Link>
-      </button>
-      <br />
-      <button>
-        <Link href="/patients">Patients</Link>
-      </button>
-      <br />
-    </main>
+    <div>
+      <div>
+        <nav className="bg-gray-800">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <h1 className="text-white text-2xl font-bold">Hello</h1>
+              <ul className="flex space-x-4 text-white">
+                <li>
+                  <Link href="/users">Users</Link>
+                </li>
+                <li>
+                  <Link href="/doctors">Doctors</Link>
+                </li>
+                <li>
+                  <Link href="/patients">Patients</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div>
+        {doctorsData &&
+          doctorsData.datasets.map((data, index) => (
+            <canvas
+              key={index}
+              ref={(ref) => (chartRefs.current[index] = ref || {})}
+              width="400"
+              height="400"
+            />
+          ))}
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
--- admin-dashboard/src/pages/users.js
+++ admin-dashboard/src/pages/users.js
@@ -1,26 +1,91 @@
-import React, {useState,useEffect} from 'react'
-import Image from 'next/image'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+import axios from 'axios';
+import { useRouter } from "next/router";
 
-export default function users() {
-const [users,setUsers]=useState([])
-useEffect(()=>{
-  axios.get("http://localhost:8080/admin/users")
-  .then (res=>{setUsers(res.data)})
-},[])
+export default function Users() {
+  const [users, setUsers] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = () => {
+    axios
+      .get('http://localhost:8080/admin/users')
+      .then(res => {
+        setUsers(res.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const deleteUser = id => {
+    axios
+      .delete(`http://localhost:8080/user/${id}`)
+      .then(() => {
+        fetchUsers();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  const handleReturnHome = () => {
+    router.push("/");
+  };
   return (
-    <main
-    >
-     <h1>All Users</h1>
-
-{users.map((e,i)=><ul key={i}> 
-  <li>Name:{e.name}</li>
-  <li>Role:{e.role}</li>
-  <li>Email:{e.email}</li>
-  <li>Created At{e.createdAt}</li>
-  <li><button>Delete</button></li>
-  <li><button>Add as Admin</button></li>
-</ul>)}
+    <main className="bg-blue-500 ">
+    <div className="p-4 mr-4">
+      <button
+        onClick={handleReturnHome}
+        className="bg-white text-blue-500 ml-5 py-2 px-4 rounded "
+      >
+        Return to Home Page
+      </button>
+    </div>
+    <div className="bg-blue-500 min-h-screen flex flex-col items-center justify-center">
+    <h1 className="text-6xl text-white font-bold my-8">EASYMED</h1>
+      <h1 className="text-6xl text-black my-8">All Users</h1>
+      <div className="bg-white p-8 rounded-lg">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="py-2">Name</th>
+              <th className="py-2">Role</th>
+              <th className="py-2">Email</th>
+              <th className="py-2">Created At</th>
+              <th className="py-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td className="py-2">{user.name}</td>
+                <td className="py-2">{user.role}</td>
+                <td className="py-2">{user.email}</td>
+                <td className="py-2">{user.createdAt}</td>
+                <td className="py-2">
+                  <button
+                    onClick={() => deleteUser(user.id)}
+                    className="bg-red-500 text-white py-2 px-4 rounded mr-2"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => addAdmin(user.id)}
+                    className="bg-green-500 text-white py-2 px-4 rounded"
+                  >
+                    Add as Admin
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
